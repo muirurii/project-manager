61ea7252e85ebd6529a0e553b5e49b09ef078514
muirurii <pnkanuna@gmail.com>
2023-03-18 13:26:34 +0300

    configured signup validation

--- frontend/src/Pages/Signup.tsx
+++ frontend/src/Pages/Signup.tsx
@@ -1,8 +1,39 @@
-import { Button } from "react-bootstrap";
+import { Alert, Button } from "react-bootstrap";
 import {Formik,Form,Field,ErrorMessage} from "formik";
+import { useMutation  } from "@apollo/client";
+import { ADD_USER } from "../graphQL/mutations/users";
 
+interface FormTypes {
+    username: string
+    email: string
+    password: string
+    repeatPassword: string
+}
 
 const SignUp = () => {
+
+    const [mutate] = useMutation(ADD_USER);
+
+    const validate = (values:FormTypes) =>{
+        
+        const errors:any = {}
+
+        if(!values.email){
+            errors.email = "email required"
+        }
+        if(!values.username){
+            errors.username = "username required"
+        }
+        if(!values.password){
+            errors.password = "password required"
+        }
+        if(values.password !== values.repeatPassword){
+            errors.password = "passwords don't match"
+            errors.repeatPassword = "passwords don't match"
+        }
+
+        return errors;
+    }
 
   return (
     <div className="container">
@@ -15,8 +46,18 @@ const SignUp = () => {
             repeatPassword:"",
             rememberMe:false
         }}
-        onSubmit= {(values,{setSubmitting}) =>{
-            console.log(values);
+        validate={validate}
+        onSubmit= { async (values,{setSubmitting}) =>{
+
+          const {data,errors} = await mutate({variables:{
+                ...values
+            }});
+            if(errors?.length){
+                console.log("error",errors.map(e=>e.message))
+            }else{
+                console.log("success",data);
+            }
+
         }}
         >
             <Form>
@@ -30,6 +71,9 @@ const SignUp = () => {
             className="form-control"
             name ="username"
             />
+            <Alert className="p-0 text-danger mt-1" variant="light">
+            <ErrorMessage name="username" />
+            </Alert>
             </div>
             <div className="mb-3">
             <label className="form-label"
@@ -41,6 +85,9 @@ const SignUp = () => {
             className="form-control"
             name ="email"
             />
+            <Alert className="p-0 text-danger mt-1" variant="light">
+            <ErrorMessage name="email" />
+            </Alert>
             </div>
             <div className="mb-3">
                 <label className="form-label" htmlFor="password">password</label>
@@ -49,6 +96,9 @@ const SignUp = () => {
                 id="password"
                 name ="password"
                 />
+                <Alert className="p-0 text-danger mt-1" variant="light">
+            <ErrorMessage name="password" />
+            </Alert>
             </div>
             <div className="mb-3">
                 <label className="form-label" htmlFor="password">repeat password</label>
@@ -57,6 +107,9 @@ const SignUp = () => {
                 id="repeatPassword"
                 name ="repeatPassword"
                 />
+                <Alert className="p-0 text-danger mt-1" variant="light">
+            <ErrorMessage name="repeatPassword" />
+            </Alert>
             </div>
             <div className="mb-3"><div className="form-check">
                 <input type="checkbox" id="formBasicCheckbox" className="form-check-input" />
---
+++ frontend/src/graphQL/mutations/users/index.tsx
@@ -0,0 +1,21 @@
+import {gql} from "@apollo/client";
+
+const ADD_USER = gql`
+    mutation addUser(
+        $username:String!,
+        $email:String!,
+        $password:String!
+        $repeatPassword:String!
+    ) {
+        addUser(input:{username:$username,email:$email,
+        password:$password,repeatPassword:$repeatPassword}){
+            _id
+            username
+            email
+            picture,
+            createdAt
+        }
+    }
+`
+
+export {ADD_USER};   
